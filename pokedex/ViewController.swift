ec2694aba2007e8bf7cebfc52894faf703c47a8d
italomelo <italomelo@audax.mobi>
2020-08-22 03:39:30 -0300

    Tableview com custom cells (sem imagem renderizando, e lista fixa)

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct Pokemon {
+    let id: Int;
+    let name: String;
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tblPokemon: UITableView!
+    
+    var dataPokedex : [Pokemon] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tblPokemon.dataSource = self;
+        tblPokemon.delegate = self;
+        loadData();
+        
+    }
+
+    func loadData(){
+        dataPokedex.append(Pokemon(id: 1, name: "Bulbassaur"))
+        dataPokedex.append(Pokemon(id: 2, name: "Ivysaur"))
+    }
+}
+
+extension ViewController: UITableViewDataSource ,UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 83;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataPokedex.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+         guard let pokemonCell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath) as? PokemonTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let pokemon = dataPokedex[indexPath.row]
+        
+        pokemonCell.lblPokemonName.text = "\(pokemon.id) - \(pokemon.name)"
+        
+        return pokemonCell
+    }
+}
