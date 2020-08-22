682253a6c3c4b652be267bafc5725edb3a3d391f
italomelo <italomelo@audax.mobi>
2020-08-21 21:39:59 -0300

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Italo Melo on 21/08/20.
+//  Copyright © 2020 Italo Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
